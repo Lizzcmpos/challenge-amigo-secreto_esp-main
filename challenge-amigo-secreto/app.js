bbbf3716d00f80c103255ab2c6fbef941b53ce29
Lizzcmpos <natalieriver.1213@gmail.com>
2025-03-18 07:24:00 -0600

    cambiamos la opcion de inserte un nombre a una mas amigable

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -1,3 +1,46 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 let amigos = [];
+/**
+ * se definen las funciones
+ */
+function addFriend() {
+    let nameFriend = document.getElementById("friend").value; 
+
+    if (nameFriend.trim() === "") {
+        alert("Por favor, inserte el nombre de tu amigo");
+      } else {
+        amigos.push(nameFriend);
+        document.querySelector("#friend").value = "";
+        mostrarListaAmigo();
+      }
+    }
+    
+    /**
+     * Actualiza la visualización de la lista de amigos en el DOM, creando elementos <li> para cada amigo.
+     */
+    function mostrarListaAmigo() {
+      let listaAmigos = document.querySelector("#listaAmigos");
+      listaAmigos.innerHTML = "";
+    
+      for (let index = 0; index < amigos.length; index++) {
+        const element = amigos[index];
+    
+        let listaHTML = document.createElement("li");
+        listaHTML.textContent = element;
+        listaAmigos.appendChild(listaHTML);
+      }
+    }
+    
+    /**
+     * Sortea y muestra un amigo de la lista de amigos de manera aleatoria.
+     * Verifica que la lista no esté vacía antes de realizar el sorteo.
+     */
+    function sortearAmigo() {
+      let cantidadAmigos = amigos.length;
+      if (cantidadAmigos === 0) {
+        alert("Por favor, inserte un nombre antes de sortear");
+      } else {
+        let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+        let resultadoHTML = document.querySelector("#resultado");
+        resultadoHTML.innerHTML = amigos[indiceAmigo];
+      }
+    }
