acea2f1673f2ba4cd88eb79c37706241cb6e03ff
Lizzcmpos <natalieriver.1213@gmail.com>
2025-03-18 14:17:10 -0600

    se comenta la funcion addFriend

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -3,14 +3,13 @@ let amigos = [];
  * se definen las funciones
  */
 function addFriend() {
-    let nameFriend = document.getElementById("friend").value; 
+    let nameFriend = document.getElementById("friend").value;   //llama el elemento con el id friend, desde el form HTML
 
     if (nameFriend.trim() === "") {
-        alert("Por favor, inserta el nombre de tu amigo");
+        alert("Por favor, inserta el nombre de tu amigo");      //Se agrega Validacion para evitar campos 'vacios'
       } else {
         amigos.push(nameFriend);
         document.querySelector("#friend").value = "";
-        mostrarListaAmigo();
+        mostrarListaAmigo();                                    //se agregan los datos del form a la lista 
       }
     }
-    
